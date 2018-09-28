fa3af5026d157c8fda8169b7aa2fe99184942d24
ntnam1992 <ntnam1992@gmail.com>
2018-09-28 15:36:42 +0700

    update jquery.js

--- append-jquery/js/jquery.js
+++ append-jquery/js/jquery.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
 	$("button[name=btn-add]").click(function(){
 
 		var productName = $('input[name=product-name]').val();
+		console.log(productName.length);
 		if(productName == ""){
+			alert('Product Name not empty');
 			return 0;
-		}else{
-			$("ol").append("<li>"+productName+"<button>X</button>"+"</li>");
+		}
+		if(productName.length >30){
+			alert('Lenght of Name < 30 character');
+			return 0;
+		}
+			$("ol").append("<li>"+productName+"<button>X</button></li>");
 			$("li button").addClass('btn-remove');
 			$('input[name=product-name]').val("");
-		}
+		
 	});
 	$("ol").on("click",".btn-remove",function(){
 		$(this).parent('li').remove();
