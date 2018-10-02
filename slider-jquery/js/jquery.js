740f45382f618b28db6622141164ef2e4a10d438
ntnam1992 <ntnam1992@gmail.com>
2018-10-02 13:43:15 +0700

    Update slider-jquery-animate

--- slider-jquery/js/jquery.js
+++ slider-jquery/js/jquery.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
 	var btnPre = $(".js-pre");
 	var btnNext = $(".js-next")
-	var maxImg = 4
+	var maxImg = 4;
+	
 	var list; 
 	var minImg = 0;
 	var count = 0;
+	var temp = 0
+	var widthImg = $('.slider__item-info img').width();
+	var positionItem;
 	setInterval(function() {
 		// var listImg = $("slider__item-info ").eq;
 		// for( var i = 0; i < maxImg +1 ;i++)
@@ -19,48 +23,73 @@ $(document).ready(function(){
 
 	});
 	$(".slider").on("click",".js-next",function(){
-		
-		if( count == maxImg ){
-			count = 0
-			$(".slider__item-info li ").eq(maxImg).css('display','none');
-			$(".slider__item-info li ").eq(count).css('display','block');
-			$(".slider__item-nav li ").css('opacity',1);
-			$(".slider__item-nav li ").eq(count).css('opacity',0.5);
+		count++;
+		if( count > maxImg ){
+			count = 0;
+			// $(".slider__item-info li ").eq(maxImg).css('display','none');
+			// $(".slider__item-info li ").eq(count).css('display','block');
+			// $(".slider__item-nav li ").css('opacity',1);
+			// $(".slider__item-nav li ").eq(count).css('opacity',0.5);
+			animate(count);
+			$(".slider__item-info").animate({right:widthImg*(count)},600);
+
 			
 			 
 		}else{
-			$(".slider__item-info li ").eq(count).css('display','none');
-			$(".slider__item-info li ").eq(count+1).css('display','block');
-			$(".slider__item-nav li ").css('opacity',1);
-			$(".slider__item-nav li ").eq(count +1).css('opacity',0.5);
+			// $(".slider__item-info li ").eq(count).css('display','none');
+			// $(".slider__item-info li ").eq(count+1).css('display','block');
+			// $(".slider__item-nav li ").css('opacity',1);
+			// $(".slider__item-nav li ").eq(count +1).css('opacity',0.5);
+			animate(count);
+			$(".slider__item-info").animate({right:widthImg*(count)},600);
 			
 			
-			count++;
 		}	
+		
 
 	});
 	$(".slider").on("click",".js-pre",function(){
+		count--;
+		var temp = 4
+		if( count < 0 ){
+			count = maxImg
+			// $(".slider__item-info li ").eq(minImg).css('display','none');
+			// $(".slider__item-info li ").eq(count).css('display','block');
+			// $(".slider__item-nav li ").css('opacity',1);
+			// $(".slider__item-nav li ").eq(count).css('opacity',0.5);
+			
+
+		}
+
+			// $(".slider__item-info li ").eq(count).css('display','none');
+			// $(".slider__item-info li ").eq(count -1).css('display','block');
+			// $(".slider__item-nav li ").css('opacity',1);
+			// $(".slider__item-nav li ").eq(count -1).css('opacity',0.5);
+			animate(count)
+			$(".slider__item-info").animate({right:widthImg*(count)},600);
 		
-		if( count == minImg ){
-			count = 4
-			$(".slider__item-info li ").eq(minImg).css('display','none');
-			$(".slider__item-info li ").eq(count).css('display','block');
-			$(".slider__item-nav li ").css('opacity',1);
-			$(".slider__item-nav li ").eq(count).css('opacity',0.5);
-		}else{
-			$(".slider__item-info li ").eq(count).css('display','none');
-			$(".slider__item-info li ").eq(count -1).css('display','block');
-			$(".slider__item-nav li ").css('opacity',1);
-			$(".slider__item-nav li ").eq(count -1).css('opacity',0.5);
-			count--;
-		}	
+			
 	});
 	$(".slider__item-nav li").click(function(){
-		var positionItem = $(this).index();		
-		$(".slider__item-info li ").css('display','none');
-		$(".slider__item-info li").eq(positionItem).css('display','block');
-		$(".slider__item-nav li").css('opacity',1);
-		$(".slider__item-nav li").eq(positionItem).css('opacity',0.5);
+		var positionItem = $(this).index();
+		animate(positionItem);
+		count = positionItem;
+		$(".slider__item-info").animate({right:widthImg*(count)},600);		
+		// $(".slider__item-info li ").css('display','none');
+		// $(".slider__item-info li").eq(positionItem).css('display','block');
+		// $(".slider__item-nav li").css('opacity',1);
+		// $(".slider__item-nav li").eq(positionItem).css('opacity',0.5);
+		
 
 	});	
+	function animate(count){
+			
+	    // $(".slider__item-info li ").css('display','none');
+	    // $(".slider__item-info li ").eq(count).css('display','block');
+		$(".slider__item-nav li ").css('opacity',1);
+		$(".slider__item-nav li ").eq(count).css('opacity',0.5);
+		
+
+
+	}
 });
