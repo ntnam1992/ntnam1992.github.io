666ad94cecef81cb2939cef9fd4ef617f67322aa
ntnam1992 <ntnam1992@gmail.com>
2018-11-05 16:27:13 +0700

    standfood

---
+++ standfood/js/jquery.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+	var h1,h2;
+	var h0 = 0;
+	$( window ).resize(function() {
+	  	var itemWidth = $('.js-process').width();
+		itemWidth = itemWidth/2;
+		console.log(itemWidth);
+		// $('.triangle2-sp').css({borderLeft: itemWidth+'px solid transparent'});
+		// $('.triangle2-sp').css({borderRight: itemWidth+'px solid transparent'});
+		// $('.triangle1-sp').css({borderLeft: itemWidth+'px solid transparent'});
+		// $('.triangle1-sp').css({borderRight: itemWidth+'px solid transparent'});
+		$('.triangle2-sp').css({borderLeft: itemWidth +'px'});
+		$('.triangle2-sp').css({borderRight: itemWidth +'px'});
+		$('.triangle2-sp').css({borderStyle: "solid"});
+		$('.triangle2-sp').css({borderLeftColor: "transparent"});
+		$('.triangle2-sp').css({borderRightColor: "transparent"});
+
+		$('.triangle1-sp').css({borderLeft: itemWidth +'px'});
+		$('.triangle1-sp').css({borderRight: itemWidth +'px'});
+		$('.triangle1-sp').css({borderStyle: "solid"});
+		$('.triangle1-sp').css({borderLeftColor: "transparent"});
+		$('.triangle1-sp').css({borderRightColor: "transparent"});
+
+	});
+
+	$(".js-ckeck").click(function(){
+		var test = $(this).hasClass("check-act");
+		if(test == false){
+			$(".js-ckeck").removeClass('check-act');
+			$(this).addClass('check-act');
+		}
+	});
+
+	// $(".mean-nav li").click(function(){
+	// 	//if( $(this).find(".subNav").length > 0){
+	// 		console.log("aaaaaa");
+	// 	// }
+	// });
+	// $(document).on("click",".mean-nav li",function(){
+	// 	if( $(this).find(".subNav").length > 0 & !$(this).hasClass("active")){
+	// 		$(this).addClass("active")
+	// 	}
+	// 	else{
+
+	// 			$(this).removeClass("active");
+			
+	// 	}
+	// });
+	$('.js-sub').click(function(){
+		var test = $(this).hasClass('btn-change');
+		var subBtn = $(this).hasClass('btn-sub');
+		var addBtn = $(this).hasClass('btn-add');
+		var objAnimate;
+		if (subBtn== true & test == false) {
+			$(this).parent().find(".btn-change").removeClass("btn-change");
+			$(this).addClass("btn-change");
+			objAnimate = $(this).parent().parent().parent().parent().find(".content-1")
+			console.log(objAnimate);
+			objAnimate.hide()
+
+		}
+		if (addBtn== true & test == false) {
+			$(this).parent().find(".btn-change").removeClass("btn-change");
+			$(this).addClass("btn-change");
+			objAnimate = $(this).parent().parent().parent().parent().find(".content-1")
+			objAnimate.show()
+
+		}
+	});
+	function animate(obj,height){
+		$("obj").animate({height : h0},500);
+	}
+});
