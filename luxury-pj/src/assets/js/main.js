5434ac7f7ad4c62108613b751f87730c33028402
namnt <namnt@runsystem.net>
2020-03-03 17:47:54 +0700

    update luxury

---
+++ luxury-pj/src/assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(".js-check").click(function(){
+    clickCheck();
+  });
+  $( ".js-select" ).change(function() {
+    var valueOption = $(this).val();
+    var ipOption = $(this).parent().find(".js-option");
+    ipOption.text(valueOption);
+  });
+  function clickCheck(){
+    if($("#check-accept").is(":checked")){
+      $(".js-check-on,.js-check-off").removeClass("active")
+      $(".js-check-on").addClass("active")
+    }else{
+      $(".js-check-on,.js-check-off").removeClass("active")
+      $(".js-check-off").addClass("active")
+    }
+  }
+});
+
+    
