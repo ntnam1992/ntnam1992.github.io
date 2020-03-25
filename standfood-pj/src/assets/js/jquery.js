a55b660acf32986966d38e314d80ccbfec1770e9
namnt <namnt@runsystem.net>
2020-03-25 14:42:24 +0700

    update src

--- standfood-pj/src/assets/js/jquery.js
+++ standfood-pj/src/assets/js/jquery.js
@@ -1,22 +1,26 @@
 $(document).ready(function(){
 	$(".js-extend").hide();
-		$(".js-check").click(function(){
-			var subBtn = $(this).hasClass("btn__sub");
-			var addBtn = $(this).hasClass("btn__add");
-			var test = $(this).hasClass("active");
-			$(".js-check").css({"pointer-events":"none"});
-			if (subBtn== true & test == true) {
-				$(this).removeClass("active");
-				$(this).parent().parent().find(".js-add").addClass("active");
-				var tadShow = $(this).attr("data-show");
-				$("#" + tadShow).slideUp(500);
-			}
-			if (addBtn== true & test == true) {
-				$(this).removeClass("active");
-				$(this).parent().parent().find(".js-sub").addClass("active");
-				var tadShow = $(this).attr("data-show");
-				$("#" + tadShow).slideDown(500);
-			}
-			setTimeout(function(){ $(".js-check").css({"pointer-events":"auto"}); }, 400);
-		});
+	$(".js-check").click(function(){
+		var subBtn = $(this).hasClass("btn__sub");
+		var addBtn = $(this).hasClass("btn__add");
+		var test = $(this).hasClass("active");
+		$(".js-check").css({"pointer-events":"none"});
+		if (subBtn== true & test == true) {
+			$(this).removeClass("active");
+			$(this).parent().parent().find(".js-add").addClass("active");
+			var tadShow = $(this).attr("data-show");
+			$("#" + tadShow).slideUp(500);
+		}
+		if (addBtn== true & test == true) {
+			$(this).removeClass("active");
+			$(this).parent().parent().find(".js-sub").addClass("active");
+			var tadShow = $(this).attr("data-show");
+			$("#" + tadShow).slideDown(500);
+		}
+		setTimeout(function(){ $(".js-check").css({"pointer-events":"auto"}); }, 400);
 	});
+	$(".nav__menu > ul > li").click(function(){
+		$(".nav__menu > ul > li").removeClass("active");
+		$(this).addClass("active");
+	})
+});
