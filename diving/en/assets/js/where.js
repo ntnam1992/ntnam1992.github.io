f6558c20cd1d855dd78a2292f3fe1ba8948cd189
namnt <namnt@runsystem.net>
2020-04-09 11:01:28 +0700

    update code page where

---
+++ diving/en/assets/js/where.js
@@ -0,0 +1,390 @@
+var dataAPI = {
+  "akita":{
+     "id":"6253",
+     "areas":{
+        "Akita- Oga Peninsula":"6252"
+     }
+  },
+  "chiba":{
+     "id":"6102",
+     "areas":{
+        "Chiba-Chiba Ito":"6155",
+        "Chiba-Okinoshima":"6376",
+        "Chiba-Handa":"6373",
+        "Chiba-Hazama Underwater Park":"6374",
+        "Chiba-Kamogawa":"6372",
+        "Chiba-Katsuura":"6170",
+        "Chiba-Namegawa":"6375",
+        "Chiba-Nishikawana":"6171"
+     }
+  },
+  "ehime":{
+     "id":"6312",
+     "areas":{
+        "Ehime-Akehama":"6314",
+        "Ehime-Tsushima":"6313",
+        "Ehime-Ainan":"6449"
+     }
+  },
+  "fukui":{
+     "id":"6166",
+     "areas":{
+        "Fukui-Echizen":"6167",
+        "Fukui-Minami Echizen":"6367",
+        "Fukui-Otomi":"6366"
+     }
+  },
+  "fukuoka":{
+     "id":"6198",
+     "areas":{
+        "Fukuoka-Okinoshima":"6226",
+        "Fukuoka-Futamigaura":"6426",
+        "Fukuoka-Itoshima":"6427",
+        "Fukuoka-Koinoura":"6425",
+        "Fukuoka-Shikanoshima Island":"6199"
+     }
+  },
+  "hokkaido":{
+     "id":"6151",
+     "areas":{
+        "Hokkaido-Hakodate Usujiri":"6352",
+        "Hokkaido-Shakotan":"6159",
+        "Hokkaido-Shiretoko Rausu":"6273",
+        "Hokkaido-Shiretoko Utoro":"6182"
+     }
+  },
+  "hyogo":{
+     "id":"6302",
+     "areas":{
+        "Hyogo-Takeno":"6304"
+     }
+  },
+  "ishikawa":{
+     "id":"6164",
+     "areas":{
+        "Ishikawa-Noto Island":"6165"
+     }
+  },
+  "kagawa":{
+     "id":"6301",
+     "areas":{
+        "Kagawa-Setouchi":"6317"
+     }
+  },
+  "kagoshima":{
+     "id":"6111",
+     "areas":{
+        "Kagoshima-Amami Oshima Center area":"6211",
+        "Kagoshima-Amami Oshima North":"6210",
+        "Kagoshima-Amami Oshima South area":"6212",
+        "Kagoshima-Yakushima":"6208",
+        "Kagoshima-Yoronjima":"6214",
+        "Kagoshima-Bonotsu":"6205",
+        "kagoshima-Iwojima Island":"6437",
+        "Kagoshima-Kuchinoshima":"6209",
+        "Kagoshima-Kutajima":"6435",
+        "Kagoshima-Okinoerabujima":"6213",
+        "Kagoshima-Sakurajima Island Kinkowan Bay":"6206",
+        "Kagoshima-Tanegashima Island":"6438",
+        "Kagoshima-Tokunoshima Island":"6439",
+        "Kagoshima-Yamakawaoki Kamise":"6434"
+     }
+  },
+  "kanagawa":{
+     "id":"6103",
+     "areas":{
+        "Kanagawa-Hayakawa":"6380",
+        "Kanagawa-Hayama":"6174",
+        "Kanagawa-Ishibashi":"6382",
+        "Kanagawa-Jogashima":"6172",
+        "Kanagawa-Enoshima/Eboshi iwa":"6283",
+        "Kanagawa-Fukuura":"6378",
+        "Kanagawa-Iwa":"6383",
+        "Kanagawa-Mitohama":"6379",
+        "Kanagawa-Moroiso":"6377",
+        "Kanagawa-Nebukawa":"6381"
+     }
+  },
+  "kochi":{
+     "id":"6195",
+     "areas":{
+        "Kochi-Otsuki Kashiwajima":"6196",
+        "Kochi-Amaji":"6418",
+        "Kochi-Issai":"6417",
+        "Kochi-Kannoura":"6424",
+        "Kochi-Kashinishi Beach":"6420",
+        "Kochi-Muroto":"6423",
+        "Kochi-Okitsu":"6421",
+        "Kochi-Shiwa":"6422",
+        "Kochi-Tachibanaura":"6419",
+        "Kochi-Tatsukushi/Ashizuri":"6308",
+        "Kochi-Uguru Island Okinoshima":"6197"
+     }
+  },
+  "kumamoto":{
+     "id":"6203",
+     "areas":{
+        "Kumamoto-Amakusa/Ushibuka":"6204"
+     }
+  },
+  "kyoto":{
+     "id":"6370",
+     "areas":{
+        "Kyoto-Arashiyama Seiryugawa river":"6462",
+        "Kyoto-Kanmurijima":"6371",
+        "Kyoto-Kumogahata(Kamogawa river)":"6461"
+     }
+  },
+  "mie":{
+     "id":"6106",
+     "areas":{
+        "Mie-Kuki":"6318",
+        "Mie-Owase":"6189",
+        "Mie-Huzaura":"6408",
+        "Mie-Koka":"6410",
+        "Mie-Mikiura":"6406",
+        "Mie-Nansei":"6409"
+     }
+  },
+  "miyagi":{
+     "id":"6257",
+     "areas":{
+        "Miyagi-Onagawa-Takeura":"6288",
+        "Miyagi-Shizukawa":"6229"
+     }
+  },
+  "miyazaki":{
+     "id":"6108",
+     "areas":{
+        "Miyazaki-Nichinan":"6207",
+        "Miyazaki-Nobeoka":"6289"
+     }
+  },
+  "nagasaki":{
+     "id":"6109",
+     "areas":{
+        "Nagasaki-Omura Bay":"6454",
+        "Nagasaki-Goto Island Kami goto":"6201",
+        "Nagasaki-Goto Island Shimo goto":"6202",
+        "Nagasaki-Ikinoshima":"6428",
+        "Nagasaki-Kujukushima Island":"6200",
+        "Nagasaki-Tatsunokuchi":"6429"
+     }
+  },
+  "niigata":{
+     "id":"6161",
+     "areas":{
+        "Niigata-Sadoga Island":"6162",
+        "Niigata-Kashiwazaki":"6368",
+        "Niigata-Oyashirazu":"6163"
+     }
+  },
+  "oita":{
+     "id":"6275",
+     "areas":{
+        "Oita-Fukajima":"6276",
+        "Oita-Hazako":"6433",
+        "Oita-Kamae":"6431",
+        "Oita-Saganoseki":"6430",
+        "Oita-Zeai":"6432"
+     }
+  },
+  "okinawa":{
+     "id":"6110",
+     "areas":{
+        "Okinawa-Agunijima Island":"6356",
+        "Okinawa-Chibishi Islands":"6216",
+        "Okinawa-Iriomotejima Island":"6222",
+        "Okinawa-Ishigakijima Island":"6220",
+        "Okinawa-Kerama Islands":"6217",
+        "Okinawa-Kumejima Island":"6218",
+        "Okinawa-Miyakojima Island":"6219",
+        "Okinawa-Motobu Peninsula":"6215",
+        "Okinawa-Onna Village":"6153",
+        "Okinawa-Yonagunijima Island":"6221",
+        "Okinawa-Ginoza":"6441",
+        "Okinawa-Iejima Island":"6440",
+        "Okinawa-Irabujima Island/Shimojishima Island":"6445",
+        "Okinawa-Kinwan Bay":"6442",
+        "Okinawa-Kohamajima Island":"6448",
+        "Okinawa-Sunabe":"6443",
+        "Okinawa-Taketomijima Island":"6447",
+        "Okinawa-Taramajima Island":"6446",
+        "Okinawa-Tonakijima Island":"6444"
+     }
+  },
+  "saga":{
+     "id":"6291",
+     "areas":{
+        "Saga-Karatsu":"6292"
+     }
+  },
+  "shimane":{
+     "id":"6193",
+     "areas":{
+        "Shimane-Oki Okinoshima Island":"6416",
+        "Shimane-Shimane Ota":"6194"
+     }
+  },
+  "shizuoka":{
+     "id":"6104",
+     "areas":{
+        "Shizuoka-Ito":"6333",
+        "Shizuoka-Futo":"6177",
+        "Shizuoka-Hokkawa":"6179",
+        "Shizuoka-IOP":"6178",
+        "Shizuoka-Ita":"6227",
+        "Shizuoka-Kawana":"6176",
+        "Shizuoka-Koganezaki":"6183",
+        "Shizuoka-Kumomi":"6185",
+        "Shizuoka-Mikomoto Island":"6186",
+        "Shizuoka-Osezaki":"6154",
+        "Shizuoka-Shishihama":"6180",
+        "Shizuoka-Shizuura":"6398",
+        "Shizuoka-Tago":"6184",
+        "Shizuoka-Atagawa":"6390",
+        "Shizuoka-Atami":"6286",
+        "Shizuoka-Dogashima":"6396",
+        "Shizuoka-Hagachizaki":"6400",
+        "Shizuoka-Hatsushima":"6175",
+        "Shizuoka-Hirasawa":"6394",
+        "Shizuoka-Inatori":"6391",
+        "Shizuoka-Izusan":"6386",
+        "Shizuoka-Mera":"6388",
+        "Shizuoka-Nakagi":"6402",
+        "Shizuoka-Okawa":"6389",
+        "Shizuoka-Senbon Hama":"6397",
+        "Shizuoka-Shoubuzawa":"6393",
+        "Shizuoka-Suzaki":"6401",
+        "Shizuoka-Toi":"6395",
+        "Shizuoka-Ukishima":"6399",
+        "Shizuoka-Usami":"6387",
+        "Shizuoka-Yazu":"6392"
+     }
+  },
+  "tokushima":{
+     "id":"6299",
+     "areas":{
+        "Tokushima-Mugi":"6311",
+        "Tokushima-Shishikuicho":"6310"
+     }
+  },
+  "tokyo":{
+     "id":"6187",
+     "areas":{
+        "Tokyo-Hachijojima Island":"6281",
+        "Tokyo-Izu Oshima":"6188",
+        "Tokyo-Ogasawara Islands":"6284",
+        "Tokyo-Kozujima Island":"6385",
+        "Tokyo-Miyakejima Island":"6278",
+        "Tokyo-Shikinejima Island":"6384"
+     }
+  },
+  "tottori":{
+     "id":"6303",
+     "areas":{
+        "Tottori-Tajiri":"6305"
+     }
+  },
+  "toyama":{
+     "id":"6168",
+     "areas":{
+        "Toyama-Namerikawa":"6169",
+        "Toyama-Fushikikokubu Kaigan":"6369"
+     }
+  },
+  "wakayama":{
+     "id":"6107",
+     "areas":{
+        "Wakayama - Kushimoto":"6113",
+        "Wakayama - Tanabe":"6115",
+        "Wakayama -Shirahama":"6112",
+        "Wakayama-Koza":"6228",
+        "Wakayama-Kii Oshima(Sue)":"6407",
+        "Wakayama-Minabe":"6404",
+        "Wakayama-Shirasaki":"6403",
+        "Wakayama-Susami":"6405"
+     }
+  },
+  "yamagata":{
+     "id":"6223",
+     "areas":{
+        "Yamagata-Kamo":"6364",
+        "Yamagata-Ryutoanjima(Yotsushima)":"6365",
+        "Yamagata-Shonai":"6224",
+        "Yamagata-Tobishima":"6363"
+     }
+  },
+  "yamaguchi":{
+     "id":"6190",
+     "areas":{
+        "Yamaguchi-Oomijima":"6192",
+        "Yamaguchi-Suou-Oshima":"6297",
+        "Yamaguchi-Agawa":"6296",
+        "Yamaguchi-Aishima":"6414",
+        "Yamaguchi-Futaoijima Island":"6294",
+        "Yamaguchi-Mishima":"6191",
+        "Yamaguchi-Murotsu":"6415",
+        "Yamaguchi-Yoshimo":"6295"
+     }
+  }
+};
+// import { data } from './demo-api';
+$(document).ready(function() {
+  var dataSelect2 = [];
+  //get data id and key in object dataAPI
+  $.each(dataAPI, function (i, data) {
+    //push value id and key in array dataSelect2
+    // i = id,subData = area name
+    dataSelect2.push({
+      id: data.id,
+      text: i
+    });
+    //get data id and key in object dataAPI
+    $.each(dataAPI[i].areas, function (j, subData) {
+    //push value id and key in array dataSelect2
+    // j = id,subData = area name
+      dataSelect2.push({
+        id:"sub-"+ subData,
+        text: j
+      });
+    });
+  });
+
+
+
+//custom select2 key has id and idSub
+  function formatState (state) {
+    var $state = "";
+    if (state.id && state.id.indexOf("sub-") == -1 ) {
+      $state = $(
+        '<span style="font-weight:bold">' + state.text + '</span>'
+      );
+    }else{
+      $state = $(
+        '<span class="sub-item" style="margin-left:10px;display:block;">' + state.text + '</span>'
+      );
+    }
+    
+    return $state;
+  };
+//event change value select link url
+  $('.js-destination').on('select2:select', function (e) {
+    //get id value when change select option
+    var data = e.params.data.id;
+    // check value id and link to url
+    if (data && data.indexOf("sub-") == -1 ){
+      window.location.href = "https://ntnam1992.github.io/diving/en/guides/chiba/";
+    }
+    else{
+      window.location.href = "https://ntnam1992.github.io/diving/en/where/detail"
+    }
+});
+// call function select2 and import data
+  $(".js-destination").select2({
+    placeholder: 'Enter destination',
+    data: dataSelect2,//data import array dataSelect2
+    templateResult: formatState
+  });
+ 
+});
